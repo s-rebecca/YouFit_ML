1460746327955029ccf1ad949cb7ad4bf765182c
s-rebecca <samyarjv2003@gmail.com>
2024-06-11 22:50:06 +0530

    second commit

--- s.js
+++ s.js
@@ -1,6 +1,6 @@
 try{
     const a = parseInt('1697913000000')
-    console.log(new Date(parseInt('1697913000000')).toLocaleString())
+    console.log(new Date(parseInt('16979130000006')).toLocaleString())
 
 }
 catch(error){
